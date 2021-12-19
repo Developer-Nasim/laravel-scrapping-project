53a5f7a2bdfc25e03f790877c7eab7270c1b13cd
Developer-Nasim <ajnasim72@gmail.coom>
2021-12-20 05:15:20 +0600

    fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,15 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+  
+        <style>
+            
+.top-right.links {
+    width: 100%;
+    text-align: center;
+    padding: 40px;
+}
+        </style>
  
     </head>
     <body>
@@ -29,36 +36,9 @@
             @endif
 
  
-            <form action="{{url('link')}}" method="post">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="url" name="link" placeholder="link" id="" require>
-                <button type="submit">Submit</button>
-            </form>
-
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Member Price</th>
-                        <th scope="col">Created</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr> 
-                </tbody>
-            </table>
- 
 
         </div>
 
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+ 
     </body>
 </html>
